94c473c266fb93e9b392150d93e4b365e26ce9a5
SolarisTKW <mcng555@gmail.com>
2019-12-09 17:17:20 -0500

    More styling and formatting for edit Screen

--- src/components/edit_screen/EditScreen.js
+++ src/components/edit_screen/EditScreen.js
@@ -5,11 +5,31 @@ import { compose } from 'redux';
 import { firestoreConnect } from 'react-redux-firebase';
 import { getFirestore } from 'redux-firestore';
 import { FormInput, Button, Container, Col, Row , Card} from "shards-react";
-import { Link } from 'react-router-dom';
+import { PhotoshopPicker } from 'react-color';
 
 class EditScreen extends Component {
     state = {
+        showColorPickerBackground: false,
+        showColorPickerBorder: false,
+        backgroundColor: "#000000",
+        borderColor: "#f0f",
         criteria: "",
+    }
+
+    setShowBackground = () => {
+        this.setState(
+            {
+                showColorPickerBackground: !this.state.showColorPickerBackground
+            }
+        )
+    }
+
+    setShowBorder = () => {
+        this.setState(
+            {
+                showColorPickerBorder: !this.state.showColorPickerBorder
+            }
+        )
     }
 
     render() {
@@ -50,7 +70,49 @@ class EditScreen extends Component {
                                         </div>
                                     </Col>
                                 </Row>
+                                
                             </Container>
+
+                            <hr className="new1"/>
+
+                            <div className="sample_wrapper">
+                                <Container className="sample_container"/>
+                                <label htmlFor="sample_container container" className="label_for_sample">
+                                    Container
+                                </label>
+                            </div>
+
+                            <div className="sample_wrapper_label">
+                                <label htmlFor="sample_label" className="black-text label">
+                                    Prompt for Input:
+                                </label>
+                                <div className="sample_label" className="sample_label">
+                                    Label
+                                </div>
+                            </div>
+
+                            <div className="sample_wrapper_button">
+                                <div>
+                                    <Button className="sample_button">
+                                        Submit
+                                    </Button>
+                                </div>
+                                <label className="label_for_sample">
+                                    Button
+                                </label>
+                            </div>
+
+                            <div className="sample_wrapper_button">
+                                <div>
+                                    <input type="filler" className="sample_input" value="Input" readOnly></input>
+                                </div>
+                                <label className="label_for_sample">
+                                    Textfield
+                                </label>
+                            </div>
+
+                            
+
                             1/3
                         </Card>
                     </Col>
@@ -61,10 +123,60 @@ class EditScreen extends Component {
                     </Col>
                     <Col center sm = "3" md="3" lg="3" >
                         <Card className="edit_panel">
-                            <label>Properties</label>
-                            <FormInput name="name" id="id" placeholder="Name"></FormInput>
-                            <label htmlFor="font_size">Font Size:</label>
-                            <FormInput name="font_size" id="font_size" placeholder="10px"></FormInput>
+                            <div className="input_container">
+                                <label>Properties</label>
+                                <FormInput name="name" id="id" placeholder="Name"></FormInput>
+                            </div>
+
+                            <hr className="new2"/>
+
+                            <div className="input_container">
+                                <label htmlFor="font_size">Font Size:</label>
+                                <FormInput name="font_size" id="font_size" placeholder="10"></FormInput>
+                            </div>
+                            
+                            <hr className="new2"/>
+
+                            <div className="input_container">
+                                <div className="color_picker_container">
+                                    <label htmlFor="color_picker_circle" className="grey-text color_picker_circle_label">Background: </label>
+                                    <div className={"btn-floating color_picker_circle"} style={{backgroundColor: this.state.backgroundColor, color: "#FFFFFF"}} onClick={this.setShowBackground}>{this.state.backgroundColor}</div>
+                                    <div>{
+                                        this.state.showColorPickerBackground ?
+                                        <PhotoshopPicker className="color_picker"/> : null
+                                    }
+                                    </div>
+                                </div>
+                            </div>
+
+                            <hr className="new2"/>
+
+                            <div className="input_container">
+                                <div className="color_picker_container">
+                                    <label htmlFor="color_picker_circle" className="grey-text color_picker_circle_label">Border Color: </label>
+                                    <div className={"btn-floating color_picker_circle"} style={{backgroundColor: this.state.borderColor, color: "#FFFFFF"}} onClick={this.setShowBorder}>{this.state.borderColor}</div>
+                                    <div>{
+                                        this.state.showColorPickerBorder ?
+                                        <PhotoshopPicker className="color_picker"/> : null
+                                    }
+                                    </div>
+                                </div>
+                            </div>
+                            
+                            <hr className="new2"/>
+
+                            <div className="input_container">
+                                <label htmlFor="border_thickness">Border Thickness:</label>
+                                <FormInput name="border_thickness" id="border_thickness" placeholder="10"></FormInput>
+                            </div>
+
+                            <hr className="new2"/>
+
+                            <div className="input_container">
+                                <label htmlFor="border_radius">Border Radius:</label>
+                                <FormInput name="border_radius" id="border_radius" placeholder="10"></FormInput>
+                            </div>
+                            
                             3/3
                         </Card>
                     </Col>
